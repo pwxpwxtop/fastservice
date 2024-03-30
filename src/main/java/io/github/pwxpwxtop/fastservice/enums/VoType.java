4f63c5e140fe6fe6e494d9d7b0c4e73f162d41ff
2207310754@qq,com <pwx2207310754>
2024-03-30 13:18:27 +0800

    添加了一些查询属性

--- src/main/java/io/github/pwxpwxtop/fastservice/enums/VoType.java
+++ src/main/java/io/github/pwxpwxtop/fastservice/enums/VoType.java
@@ -7,6 +7,11 @@ public enum VoType {
     LIKE,//全模糊查询
     LIKE_LEFT,//左模糊查询
     LIKE_RIGHT,//右模糊查询
+    NOT_LIKE,//反向模糊匹配
+    IN,//包含
+    NOT_IN,//不包含
+    EXISTS,//存在
+    NOT_EXISTS,//不存在
     LT, //小于查询
     LE, //小于等于查询
     GT, //大于查询
