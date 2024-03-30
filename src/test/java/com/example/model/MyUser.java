3e26acc9c01b028f53a66451b40f3808112c2554
2207310754@qq,com <pwx2207310754>
2024-03-30 13:20:33 +0800

    修改了测试属性

--- src/test/java/com/example/model/MyUser.java
+++ src/test/java/com/example/model/MyUser.java
@@ -9,7 +9,7 @@ import io.github.pwxpwxtop.fastservice.animation.Vo;
 import io.github.pwxpwxtop.fastservice.enums.BoType;
 import io.github.pwxpwxtop.fastservice.enums.VoType;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+
 
 import java.util.Map;
 
@@ -25,16 +25,19 @@ import java.util.Map;
 @TableName("my_user")
 public class MyUser extends BaseModel{
 
-    @Bo(type = { BoType.NOT_STR, BoType.FILTER})
+    @Bo(type = { BoType.NOT_NULL_STR, BoType.FILTER, BoType.FILTER})
+    @Vo(type = {VoType.LIKE})
     @ExcelProperty(value = "姓名")
     private String name;
 
     @Bo(regex = "([1-9][0-9]{0,1}|100|0)", msg = "不在年龄范围内")
-    @Vo(type = {VoType.GT, VoType.NOT_NULL}, regex = "([1-9][0-9]{0,1}|100|0)", msg = "不在年龄范围内")
     @ExcelProperty(value = "年龄")
     private Integer age;
 
-    @Bo(type = { BoType.REPEAT}, regex = "^1[3-9]\\d{9}$", msg = "手机号码不正确")
+    @ExcelProperty("性别")
+    private String sex;
+
+
     @ExcelProperty(value = "手机号码")
     private Long phone;
 
@@ -44,7 +47,7 @@ public class MyUser extends BaseModel{
     @ExcelIgnore
     private Integer deleteState;
 
-    @Bo(type = { BoType.NOT_STR}, exist = false)
+    @Bo(type = { BoType.NOT_NULL_STR}, exist = false)
     @TableField(exist = false)
     @ExcelIgnore//忽略这个字段
     private Map<String, Object> map;
