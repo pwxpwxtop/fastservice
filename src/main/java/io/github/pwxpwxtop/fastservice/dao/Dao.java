60e8e00a0de04796927605d9b7a422c15587c967
2207310754@qq,com <pwx2207310754>
2024-03-30 13:17:43 +0800

    修改了dao的查询问题

--- src/main/java/io/github/pwxpwxtop/fastservice/dao/Dao.java
+++ src/main/java/io/github/pwxpwxtop/fastservice/dao/Dao.java
@@ -4,10 +4,11 @@ package io.github.pwxpwxtop.fastservice.dao;
 
 import org.apache.ibatis.annotations.SelectProvider;
 import java.util.List;
+import java.util.Map;
 
 
 public interface Dao {
 
     @SelectProvider(value = DaoProvider.class, method = "sql")
-    List<Object> sql(String sql);
+    List<Map<String, Object>> sql(String sql);
 }
