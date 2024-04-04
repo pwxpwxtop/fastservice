cac5e659fc358ea2ca481f5d11936f090e82c109
2207310754@qq,com <pwx2207310754>
2024-04-04 16:52:14 +0800

    1.2.0添加更新

--- src/test/java/com/example/mapper/MyUserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.model.MyUser;
-
-public interface MyUserMapper extends BaseMapper<MyUser> {
-}
